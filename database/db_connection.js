1100dfaa866f09d6d3fabce6af44a071cf9a8154
Larysa Ambartsumian <hi.ambalari@gmail.com>
2025-09-01 12:11:48 +0200

    added README, .env.example

--- database/db_connection.js
+++ database/db_connection.js
@@ -1,9 +1,15 @@
+import "dotenv/config";
 import { Sequelize } from "sequelize";
-const db_connection = new Sequelize('butterfly_app','root','LARIdavi1950!',{
-        host: 'localhost',
-        dialect: 'mysql',
-        define: {
-            timestamps: false //esta parte es un añadido por lo de createAT y updateAt
-        }
-}) 
+
+const db_connection = new Sequelize(
+  process.env.DB_NAME, // butterfly_app
+  process.env.DB_USER, // root
+  process.env.DB_PASSWORD ?? "", // "" si está vacío
+  {
+    host: process.env.DB_HOST || "localhost",
+    dialect: process.env.DB_DIALECT || "mysql",
+    define: { timestamps: false },
+  }
+);
+
 export default db_connection;
